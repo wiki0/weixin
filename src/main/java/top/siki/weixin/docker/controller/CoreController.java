ff845a7103af6a21487bff344fa7c468eb654017
wiki0 <wiki0@live.cn>
2017-11-04 00:17:16 +0800

    reponse

--- src/main/java/top/siki/weixin/docker/controller/CoreController.java
+++ src/main/java/top/siki/weixin/docker/controller/CoreController.java
@@ -1,4 +1,41 @@
 package top.siki.weixin.docker.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import top.siki.weixin.docker.service.CoreService;
+import top.siki.weixin.docker.util.SignUtil;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("")
 public class CoreController {
+    @Autowired
+    private CoreService coreService;
+
+    //增加日志
+    private static Logger log = LoggerFactory.getLogger(CoreController.class);
+    //验证是否来自微信服务器的消息
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public String checkSignature(@RequestParam(name = "signature" ,required = false) String signature  ,
+                                 @RequestParam(name = "nonce",required = false) String  nonce ,
+                                 @RequestParam(name = "timestamp",required = false) String  timestamp ,
+                                 @RequestParam(name = "echostr",required = false) String  echostr){
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+            log.info("接入成功");
+            return echostr;
+        }
+        log.error("接入失败");
+        return "";
+    }
+    // 调用核心业务类接收消息、处理消息跟推送消息
+    @RequestMapping(value = "",method = RequestMethod.POST)
+    public  String post(HttpServletRequest req){
+        String respMessage = coreService.processRequest(req);
+        return respMessage;
+    }
 }
