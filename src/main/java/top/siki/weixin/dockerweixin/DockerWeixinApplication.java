902af87511ce94991957c659afe696a26b7edea1
yangfan <shiki0@qq.com>
2017-11-03 17:07:21 +0800

    weixin

---
+++ src/main/java/top/siki/weixin/dockerweixin/DockerWeixinApplication.java
@@ -0,0 +1,12 @@
+package top.siki.weixin.dockerweixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerWeixinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerWeixinApplication.class, args);
+	}
+}
---
+++ src/main/java/top/siki/weixin/dockerweixin/handler/ScanHandler.java
@@ -0,0 +1,8 @@
+package top.siki.weixin.dockerweixin.handler;
+
+/**
+ * @author Binary Wang(https://github.com/binarywang)
+ */
+public abstract class ScanHandler extends AbstractHandler {
+
+}
