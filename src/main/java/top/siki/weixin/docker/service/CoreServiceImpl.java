403fc17dc132d20c7b0238f31054dc3616235243
yangfan <shiki0@qq.com>
2017-11-13 16:10:11 +0800

    改订阅提示

--- src/main/java/top/siki/weixin/docker/service/CoreServiceImpl.java
+++ src/main/java/top/siki/weixin/docker/service/CoreServiceImpl.java
@@ -363,10 +363,10 @@ public class CoreServiceImpl implements CoreService {
                     Article article = new Article();
                     article.setTitle("谢谢您的关注！");
                     // 图文消息中可以使用QQ表情、符号表情
-                    article.setDescription("点击图文可以跳转到百度首页");
+                    article.setDescription("主要提供面向人工智能服务");
                     // 将图片置为空
-                    article.setPicUrl("http://www.sinaimg.cn/dy/slidenews/31_img/2016_38/28380_733695_698372.jpg");
-                    article.setUrl("http://www.baidu.com");
+                    article.setPicUrl("http://www.wiki2link.top/bown.png");
+                    article.setUrl("http://www.wiki2link.cn");
                     articleList.add(article);
                     newsMessage.setArticleCount(articleList.size());
                     newsMessage.setArticles(articleList);
