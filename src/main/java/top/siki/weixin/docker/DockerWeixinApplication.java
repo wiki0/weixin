8db0a44e19abf0e45fcaaeb2c201dd0de1c931d0
yangfan <shiki0@qq.com>
2017-11-13 16:03:18 +0800

    加入定时任务和菜单

--- src/main/java/top/siki/weixin/docker/DockerWeixinApplication.java
+++ src/main/java/top/siki/weixin/docker/DockerWeixinApplication.java
@@ -2,8 +2,10 @@ package top.siki.weixin.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DockerWeixinApplication {
 
     public static void main(String[] args) {
