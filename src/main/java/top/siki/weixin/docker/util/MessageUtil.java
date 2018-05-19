3fa4a4674a912fad116613ddb347e469b547f92d
wiki0@live.cn <wiki2link>
2018-05-19 22:40:27 +0800

    update

--- src/main/java/top/siki/weixin/docker/util/MessageUtil.java
+++ src/main/java/top/siki/weixin/docker/util/MessageUtil.java
@@ -165,6 +165,7 @@ public class MessageUtil {
      *
      * @date 2013-05-19
      */
+    @SuppressWarnings("unchecked")
     private static XStream xstream = new XStream(new XppDriver() {
         @Override
         @SuppressWarnings("unchecked")
@@ -173,10 +174,12 @@ public class MessageUtil {
                 // 对所有xml节点的转换都增加CDATA标记
                 boolean cdata = true;
 
+                @Override
                 public void startNode(String name, Class clazz) {
                     super.startNode(name, clazz);
                 }
 
+                @Override
                 protected void writeText(QuickWriter writer, String text) {
                     if (cdata) {
                         writer.write("<![CDATA[");
