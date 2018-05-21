3fbe287fb7c0a8645fff036b86d1ee80a0721929
yangfan <wiki0@live.cn>
2018-05-21 17:34:16 +0800

    更新

--- src/main/java/top/siki/weixin/docker/service/CoreServiceImpl.java
+++ src/main/java/top/siki/weixin/docker/service/CoreServiceImpl.java
@@ -24,6 +24,7 @@ import java.util.regex.Pattern;
 
 /**
  * 核心服务类
+ *
  * @author wiki
  */
 @Slf4j
@@ -38,7 +39,7 @@ public class CoreServiceImpl implements CoreService {
      * @return
      */
     @Override
-    public  String processRequest(HttpServletRequest request) {
+    public String processRequest(HttpServletRequest request) {
 
         String respMessage = null;
         try {
@@ -77,83 +78,47 @@ public class CoreServiceImpl implements CoreService {
             String content = requestMap.get("Content");
             // 自动回复文本消息
             if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_TEXT)) {
-                //如果用户发送表情，则回复同样表情。
-                if (isQqFace(content)) {
-                    respContent = content;
-                    textMessage.setContent(respContent);
-                    respMessage = MessageUtil.textMessageToXml(textMessage);
-                } else {
-                    //回复固定消息
-                    switch (content) {
-                        case "1": {
-                            StringBuffer buffer = new StringBuffer();
-                            buffer.append("测试：发送图片获得描述").append("\n\n");
-                            buffer.append("或者您可以尝试发送表情").append("\n\n");
-                            buffer.append("回复“1”显示此帮助菜单").append("\n");
-                            respContent = String.valueOf(buffer);
-                            textMessage.setContent(respContent);
-                            respMessage = MessageUtil.textMessageToXml(textMessage);
-                            break;
-                        }
-                        case "12": {
-                            //多图文发送
-                            Article article = new Article();
-                            article.setTitle("紧急通知，不要捡这种钱！湛江都已经传疯了！\n");
-                            article.setDescription("");
-                            article.setPicUrl("http://www.sinaimg.cn/dy/slidenews/31_img/2016_38/28380_733695_698372.jpg");
-                            article.setUrl("http://mp.weixin.qq.com/s?__biz=MjM5Njc2OTI4NQ==&mid=2650924309&idx=1&sn=8bb6ae54d6396c1faa9182a96f30b225&chksm=bd117e7f8a66f769dc886d38ca2d4e4e675c55e6a5e01e768b383f5859e09384e485da7bed98&scene=4#wechat_redirect");
+                //回复固定消息
+                switch (content) {
+                    case "1": {
+                        StringBuffer buffer = new StringBuffer();
+                        buffer.append("测试：发送图片获得描述").append("\n\n");
+                        buffer.append("或者您可以尝试发送表情").append("\n\n");
+                        buffer.append("回复“1”显示此帮助菜单").append("\n");
+                        respContent = String.valueOf(buffer);
+                        textMessage.setContent(respContent);
+                        respMessage = MessageUtil.textMessageToXml(textMessage);
+                        break;
+                    }
+                    case "00": {
+                        //查询更新版本
+                        File f = new File("/app.jar");
+                        Calendar cal = Calendar.getInstance();
+                        long time = f.lastModified();
+                        cal.setTimeInMillis(time);
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss ");
+                        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+                        respContent = sdf.format(cal.getTime());
+                        textMessage.setContent(respContent);
+                        respMessage = MessageUtil.textMessageToXml(textMessage);
+                        break;
+                    }
 
-                            Article article2 = new Article();
-                            article2.setTitle("湛江谁有这种女儿，请给我来一打！");
-                            article2.setDescription("");
-                            article2.setPicUrl("http://www.sinaimg.cn/dy/slidenews/31_img/2016_38/28380_733695_698372.jpg");
-                            article2.setUrl("http://mp.weixin.qq.com/s?__biz=MjM5Njc2OTI4NQ==&mid=2650924309&idx=2&sn=d7ffc840c7e6d91b0a1c886b16797ee9&chksm=bd117e7f8a66f7698d094c2771a1114853b97dab9c172897c3f9f982eacb6619fba5e6675ea3&scene=4#wechat_redirect");
-
-                            Article article3 = new Article();
-                            article3.setTitle("以上图片我就随意放了");
-                            article3.setDescription("");
-                            article3.setPicUrl("http://www.sinaimg.cn/dy/slidenews/31_img/2016_38/28380_733695_698372.jpg");
-                            article3.setUrl("http://mp.weixin.qq.com/s?__biz=MjM5Njc2OTI4NQ==&mid=2650924309&idx=3&sn=63e13fe558ff0d564c0da313b7bdfce0&chksm=bd117e7f8a66f7693a26853dc65c3e9ef9495235ef6ed6c7796f1b63abf1df599aaf9b33aafa&scene=4#wechat_redirect");
-
-                            articleList.add(article);
-                            articleList.add(article2);
-                            articleList.add(article3);
-                            newsMessage.setArticleCount(articleList.size());
-                            newsMessage.setArticles(articleList);
-                            respMessage = MessageUtil.newsMessageToXml(newsMessage);
-                            break;
-                        }
-
-                        case "00": {
-                            //查询更新版本
-                            File f = new File("/app.jar");
-                            Calendar cal = Calendar.getInstance();
-                            long time = f.lastModified();
-                            cal.setTimeInMillis(time);
-                            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd kk:mm:ss ");
-                            sdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
-                            respContent = sdf.format(cal.getTime());
-                            textMessage.setContent(respContent);
-                            respMessage = MessageUtil.textMessageToXml(textMessage);
-                            break;
-                        }
-
-                        default: {
-                            log.info("{} 用户发送了:{}",fromUserName,content);
-                            respContent = "很抱歉，时无法提供此功能给您使用。\n\n回复“1”显示帮助信息";
-                            textMessage.setContent(respContent);
-                            // 将文本消息对象转换成xml字符串
-                            respMessage = MessageUtil.textMessageToXml(textMessage);
-                        }
+                    default: {
+                        log.info("{} 用户发送了:{}", fromUserName, content);
+                        respContent = "很抱歉，时无法提供此功能给您使用。\n\n回复“1”显示帮助信息";
+                        textMessage.setContent(respContent);
+                        // 将文本消息对象转换成xml字符串
+                        respMessage = MessageUtil.textMessageToXml(textMessage);
                     }
                 }
             }
             // 图片消息
             else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_IMAGE)) {
+                //远程调取谷歌服务
                 OkHttpClient client = new OkHttpClient();
-
                 MediaType mediaType = MediaType.parse("application/json");
-                RequestBody body = RequestBody.create(mediaType, "{\"requests\":[{\"image\":{\"source\":{\"imageUri\":\""+picUrl+"\"}},\"features\":[{\"type\":\"WEB_DETECTION\",\"maxResults\":5},{\"type\":\"SAFE_SEARCH_DETECTION\"}]}]}");
+                RequestBody body = RequestBody.create(mediaType, "{\"requests\":[{\"image\":{\"source\":{\"imageUri\":\"" + picUrl + "\"}},\"features\":[{\"type\":\"WEB_DETECTION\",\"maxResults\":5},{\"type\":\"SAFE_SEARCH_DETECTION\"}]}]}");
                 Request request2 = new Request.Builder()
                         .url("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDmPxnCgbegDGs4eO8eG0Ww7C2vXq3fMac")
                         .post(body)
@@ -161,9 +126,8 @@ public class CoreServiceImpl implements CoreService {
                         .addHeader("cache-control", "no-cache")
                         .addHeader("postman-token", "33546e9d-8c2a-fb75-e955-3be6796f0767")
                         .build();
-
                 Response response = client.newCall(request2).execute();
-                String res = response.body().string();
+                String res = response.body().toString();
                 //先转JsonObject
                 JsonObject jsonObject = new JsonParser().parse(res).getAsJsonObject();
                 //再转JsonArray 加上数据头
@@ -172,34 +136,34 @@ public class CoreServiceImpl implements CoreService {
                 JsonObject webDetection = object.get("webDetection").getAsJsonObject();
                 JsonObject safeSearchAnnotation = object.get("safeSearchAnnotation").getAsJsonObject();
                 //测试单图文回复
-
                 String uremic = "";
                 Article article = new Article();
-                if (null != webDetection.get("fullMatchingImages")){
-                    for (JsonElement object1 : webDetection.get("fullMatchingImages").getAsJsonArray()){
+                if (null != webDetection.get("fullMatchingImages")) {
+                    for (JsonElement object1 : webDetection.get("fullMatchingImages").getAsJsonArray()) {
                         article.setTitle("最佳匹配(确定)");
                         uremic = object1.getAsJsonObject().get("url").getAsString();
                     }
-                }else if (null != webDetection.get("partialMatchingImages")){
-                    for (JsonElement object1 : webDetection.get("partialMatchingImages").getAsJsonArray()){
+                } else if (null != webDetection.get("partialMatchingImages")) {
+                    for (JsonElement object1 : webDetection.get("partialMatchingImages").getAsJsonArray()) {
                         article.setTitle("部分相似(大概)");
                         uremic = object1.getAsJsonObject().get("url").getAsString();
                     }
-                }else if (null != webDetection.get("pagesWithMatchingImages")){
-                    for (JsonElement object1 : webDetection.get("pagesWithMatchingImages").getAsJsonArray()){
+                } else if (null != webDetection.get("pagesWithMatchingImages")) {
+                    for (JsonElement object1 : webDetection.get("pagesWithMatchingImages").getAsJsonArray()) {
                         article.setTitle("可能出处(不确定)");
                         uremic = object1.getAsJsonObject().get("url").getAsString();
                     }
-                }else if (null != webDetection.get("visuallySimilarImages")){
-                    for (JsonElement object1 : webDetection.get("visuallySimilarImages").getAsJsonArray()){
+                } else if (null != webDetection.get("visuallySimilarImages")) {
+                    for (JsonElement object1 : webDetection.get("visuallySimilarImages").getAsJsonArray()) {
                         article.setTitle("视觉相似(猜的)");
                         uremic = object1.getAsJsonObject().get("url").getAsString();
                     }
                 }
 
-                String picaddress = "http://47.74.153.66/api/show?path="+ makepic(uremic)+".jpg";
+                String picaddress = "http://47.74.153.66/api/show?path=" + makepic(uremic) + ".jpg";
                 article.setPicUrl(picaddress);
-                article.setUrl(uremic);
+                log.info(picaddress);
+                article.setUrl(picaddress);
 
                 StringBuilder repbody = new StringBuilder();
                 String adult = safeSearchAnnotation.getAsJsonObject().get("adult").getAsString();
@@ -207,20 +171,20 @@ public class CoreServiceImpl implements CoreService {
                 String violence = safeSearchAnnotation.getAsJsonObject().get("violence").getAsString();
 
                 if ("POSSIBLE".equals(adult) || "LIKELY".equals(adult) || "VERY_LIKELY".equals(adult))
-                    repbody.append("成人: "+toCn(safeSearchAnnotation.getAsJsonObject().get("adult").getAsString())+"\n\n");
+                    repbody.append("成人: " + toCn(safeSearchAnnotation.getAsJsonObject().get("adult").getAsString()) + "\n\n");
 
                 if ("POSSIBLE".equals(spoof) || "LIKELY".equals(spoof) || "VERY_LIKELY".equals(spoof))
-                    repbody.append(" 恶搞: "+toCn(safeSearchAnnotation.getAsJsonObject().get("spoof").getAsString())+"\n\n");
+                    repbody.append(" 恶搞: " + toCn(safeSearchAnnotation.getAsJsonObject().get("spoof").getAsString()) + "\n\n");
 
                 if ("POSSIBLE".equals(violence) || "LIKELY".equals(violence) || "VERY_LIKELY".equals(violence))
-                    repbody.append(" 暴力: "+toCn(safeSearchAnnotation.getAsJsonObject().get("violence").getAsString())+"\n\n");
+                    repbody.append(" 暴力: " + toCn(safeSearchAnnotation.getAsJsonObject().get("violence").getAsString()) + "\n\n");
 
                 repbody.append("包含内容:\n\n");
-                for (JsonElement object1 : webDetection.get("webEntities").getAsJsonArray()){
-                    repbody.append("des: "+object1.getAsJsonObject().get("description").getAsString()+" like: ");
-                    repbody.append(object1.getAsJsonObject().get("score").getAsString().substring(0,4)+"\n\n");
+                for (JsonElement object1 : webDetection.get("webEntities").getAsJsonArray()) {
+                    repbody.append("des: " + object1.getAsJsonObject().get("description").getAsString() + " like: ");
+                    repbody.append(object1.getAsJsonObject().get("score").getAsString().substring(0, 4) + "\n\n");
                 }
-                repbody.delete(repbody.length()-4,repbody.length());
+                repbody.delete(repbody.length() - 4, repbody.length());
                 article.setDescription(repbody.toString());
                 articleList.add(article);
                 newsMessage.setArticleCount(articleList.size());
@@ -237,7 +201,8 @@ public class CoreServiceImpl implements CoreService {
             }
             // 链接消息
             else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_LINK)) {
-                respContent = "您发送的是链接消息！";textMessage.setContent(respContent);
+                respContent = "您发送的是链接消息！";
+                textMessage.setContent(respContent);
                 // 将文本消息对象转换成xml字符串
                 respMessage = MessageUtil.textMessageToXml(textMessage);
             }
@@ -252,8 +217,8 @@ public class CoreServiceImpl implements CoreService {
             // 事件推送
             else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_EVENT)) {
                 // 事件类型
-                String eventType =requestMap.get("Event");
-                if(eventType.equals(MessageUtil.EVENT_TYPE_VIEW)){
+                String eventType = requestMap.get("Event");
+                if (eventType.equals(MessageUtil.EVENT_TYPE_VIEW)) {
                     // 对于点击菜单转网页暂时不做推送
                 }
                 // 订阅
@@ -267,8 +232,7 @@ public class CoreServiceImpl implements CoreService {
                     newsMessage.setArticleCount(articleList.size());
                     newsMessage.setArticles(articleList);
                     respMessage = MessageUtil.newsMessageToXml(newsMessage);
-                }
-                else if(eventType.equals(MessageUtil.EVENT_TYPE_SCAN)){
+                } else if (eventType.equals(MessageUtil.EVENT_TYPE_SCAN)) {
                     //测试单图文回复
                     Article article = new Article();
                     article.setTitle("这是已关注用户扫描二维码弹到的界面");
@@ -294,24 +258,6 @@ public class CoreServiceImpl implements CoreService {
 
         return respMessage;
     }
-    /**
-     * 判断是否是QQ表情
-     *
-     * @param content
-     * @return
-     */
-    public static boolean isQqFace(String content) {
-        boolean result = false;
-
-        // 判断QQ表情的正则表达式
-        String qqfaceRegex = "/::\\)|/::~|/::B|/::\\||/:8-\\)|/::<|/::$|/::X|/::Z|/::'\\(|/::-\\||/::@|/::P|/::D|/::O|/::\\(|/::\\+|/:--b|/::Q|/::T|/:,@P|/:,@-D|/::d|/:,@o|/::g|/:\\|-\\)|/::!|/::L|/::>|/::,@|/:,@f|/::-S|/:\\?|/:,@x|/:,@@|/::8|/:,@!|/:!!!|/:xx|/:bye|/:wipe|/:dig|/:handclap|/:&-\\(|/:B-\\)|/:<@|/:@>|/::-O|/:>-\\||/:P-\\(|/::'\\||/:X-\\)|/::\\*|/:@x|/:8\\*|/:pd|/:<W>|/:beer|/:basketb|/:oo|/:coffee|/:eat|/:pig|/:rose|/:fade|/:showlove|/:heart|/:break|/:cake|/:li|/:bome|/:kn|/:footb|/:ladybug|/:shit|/:moon|/:sun|/:gift|/:hug|/:strong|/:weak|/:share|/:v|/:@\\)|/:jj|/:@@|/:bad|/:lvu|/:no|/:ok|/:love|/:<L>|/:jump|/:shake|/:<O>|/:circle|/:kotow|/:turn|/:skip|/:oY|/:#-0|/:hiphot|/:kiss|/:<&|/:&>";
-        Pattern p = Pattern.compile(qqfaceRegex);
-        Matcher m = p.matcher(content);
-        if (m.matches()) {
-            result = true;
-        }
-        return result;
-    }
 
     public static String toCn(String content) {
         String result = content;
@@ -319,15 +265,15 @@ public class CoreServiceImpl implements CoreService {
         // 判断可能性
         if ("UNKNOWN".equals(content)) {
             result = "未知的";
-        }else if ("UNLIKELY".equals(content)) {
+        } else if ("UNLIKELY".equals(content)) {
             result = "不太像";
-        }else if ("VERY_UNLIKELY".equals(content)) {
+        } else if ("VERY_UNLIKELY".equals(content)) {
             result = "不可能";
-        }else if ("POSSIBLE".equals(content)) {
+        } else if ("POSSIBLE".equals(content)) {
             result = "可能的";
-        }else if ("LIKELY".equals(content)) {
+        } else if ("LIKELY".equals(content)) {
             result = "很像";
-        }else if ("VERY_LIKELY".equals(content)) {
+        } else if ("VERY_LIKELY".equals(content)) {
             result = "非常像";
         }
         return result;
@@ -335,13 +281,13 @@ public class CoreServiceImpl implements CoreService {
 
     public static String makepic(String geturl) throws Exception {
         String uuid = UUID.randomUUID().toString().replaceAll("-", "");
-        String path="/tmp/"+uuid+".jpg";
-        downloadPicture(geturl,path);
+        String path = "/tmp/" + uuid + ".jpg";
+        downloadPicture(geturl, path);
         return uuid;
     }
 
     //链接url下载图片
-    private static void downloadPicture(String urlList,String path) {
+    private static void downloadPicture(String urlList, String path) {
         URL url = null;
         try {
             url = new URL(urlList);
@@ -368,11 +314,10 @@ public class CoreServiceImpl implements CoreService {
 
     /**
      * 获取几小时前的时间
-     *
      */
-    public Date getBeforeHourTime(int ihour){
+    public Date getBeforeHourTime(int ihour) {
 
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd kk:mm:ss ");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss ");
         sdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) - ihour);
